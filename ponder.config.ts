e50997a8ea1bb8e5a63827bfe658f68e53fb9f24
Timur Badretdinov <accounts@destiner.io>
2024-03-18 21:03:17 +0000

    Don't index the network if the RPC URL is not set

--- ponder.config.ts
+++ ponder.config.ts
@@ -128,74 +128,322 @@ export default createConfig({
       abi: entryPoint06Abi,
       address: ENTRYPOINT_0_6_ADDRESS,
       network: {
-        ethereum: { startBlock: 17012204 },
-        goerli: { startBlock: 8801632 },
-        sepolia: { startBlock: 3257815 },
-        bsc: { startBlock: 27202729 },
-        bscTestnet: { startBlock: 28789129 },
-        polygon: { startBlock: 41335185 },
-        polygonMumbai: { startBlock: 34166156 },
-        polygonZkevm: { startBlock: 184258 },
-        polygonZkevmTestnet: { startBlock: 578113 },
-        fantom: { startBlock: 63515111 },
-        fantomTestnet: { startBlock: 15987221 },
-        opMainnet: { startBlock: 88234528 },
-        opSepolia: { startBlock: 3593614 },
-        arbitrum: { startBlock: 78720307 },
-        arbitrumSepolia: { startBlock: 623334 },
-        arbitrumNova: { startBlock: 7494602 },
-        moonriver: { startBlock: 4798917 },
-        moonbeam: { startBlock: 4026730 },
-        moonbaseAlpha: { startBlock: 4735374 },
-        cronos: { startBlock: 9728229 },
-        gnosis: { startBlock: 27364185 },
-        gnosisChiado: { startBlock: 3728232 },
-        celo: { startBlock: 21062508 },
-        celoAlfajores: { startBlock: 17649236 },
-        linea: { startBlock: 915 },
-        lineaTestnet: { startBlock: 465300 },
-        base: { startBlock: 1854367 },
-        baseSepolia: { startBlock: 1128910 },
-        scroll: { startBlock: 17 },
-        scrollSepolia: { startBlock: 91 },
-        opBnb: { startBlock: 1125290 },
-        opBnbTestnet: { startBlock: 2108065 },
-        kroma: { startBlock: 5197622 },
-        kromaTestnet: { startBlock: 7783408 },
-        avalancheC: { startBlock: 28865255 },
-        avalancheFuji: { startBlock: 20797033 },
-        zora: { startBlock: 5223236 },
-        zoraSepolia: { startBlock: 3465249 },
+        ethereum: process.env.PONDER_RPC_URL_1
+          ? {
+              startBlock: 17012204,
+            }
+          : undefined,
+        goerli: process.env.PONDER_RPC_URL_5
+          ? {
+              startBlock: 8801632,
+            }
+          : undefined,
+        sepolia: process.env.PONDER_RPC_URL_11155111
+          ? {
+              startBlock: 3257815,
+            }
+          : undefined,
+        bsc: process.env.PONDER_RPC_URL_56
+          ? {
+              startBlock: 27202729,
+            }
+          : undefined,
+        bscTestnet: process.env.PONDER_RPC_URL_97
+          ? {
+              startBlock: 28789129,
+            }
+          : undefined,
+        polygon: process.env.PONDER_RPC_URL_137
+          ? {
+              startBlock: 41335185,
+            }
+          : undefined,
+        polygonMumbai: process.env.PONDER_RPC_URL_80001
+          ? {
+              startBlock: 34166156,
+            }
+          : undefined,
+        polygonZkevm: process.env.PONDER_RPC_URL_1101
+          ? {
+              startBlock: 184258,
+            }
+          : undefined,
+        polygonZkevmTestnet: process.env.PONDER_RPC_URL_1442
+          ? {
+              startBlock: 578113,
+            }
+          : undefined,
+        fantom: process.env.PONDER_RPC_URL_250
+          ? {
+              startBlock: 63515111,
+            }
+          : undefined,
+        fantomTestnet: process.env.PONDER_RPC_URL_4002
+          ? {
+              startBlock: 15987221,
+            }
+          : undefined,
+        opMainnet: process.env.PONDER_RPC_URL_10
+          ? {
+              startBlock: 88234528,
+            }
+          : undefined,
+        opSepolia: process.env.PONDER_RPC_URL_11155420
+          ? {
+              startBlock: 3593614,
+            }
+          : undefined,
+        arbitrum: process.env.PONDER_RPC_URL_42161
+          ? {
+              startBlock: 78720307,
+            }
+          : undefined,
+        arbitrumSepolia: process.env.PONDER_RPC_URL_421614
+          ? {
+              startBlock: 623334,
+            }
+          : undefined,
+        arbitrumNova: process.env.PONDER_RPC_URL_42170
+          ? {
+              startBlock: 7494602,
+            }
+          : undefined,
+        moonriver: process.env.PONDER_RPC_URL_1285
+          ? {
+              startBlock: 4798917,
+            }
+          : undefined,
+        moonbeam: process.env.PONDER_RPC_URL_1284
+          ? {
+              startBlock: 4026730,
+            }
+          : undefined,
+        moonbaseAlpha: process.env.PONDER_RPC_URL_1287
+          ? {
+              startBlock: 4735374,
+            }
+          : undefined,
+        cronos: process.env.PONDER_RPC_URL_25
+          ? {
+              startBlock: 9728229,
+            }
+          : undefined,
+        gnosis: process.env.PONDER_RPC_URL_100
+          ? {
+              startBlock: 27364185,
+            }
+          : undefined,
+        gnosisChiado: process.env.PONDER_RPC_URL_10200
+          ? {
+              startBlock: 3728232,
+            }
+          : undefined,
+        celo: process.env.PONDER_RPC_URL_42220
+          ? {
+              startBlock: 21062508,
+            }
+          : undefined,
+        celoAlfajores: process.env.PONDER_RPC_URL_44787
+          ? {
+              startBlock: 17649236,
+            }
+          : undefined,
+        linea: process.env.PONDER_RPC_URL_59144
+          ? {
+              startBlock: 915,
+            }
+          : undefined,
+        lineaTestnet: process.env.PONDER_RPC_URL_59140
+          ? {
+              startBlock: 465300,
+            }
+          : undefined,
+        base: process.env.PONDER_RPC_URL_8453
+          ? {
+              startBlock: 1854367,
+            }
+          : undefined,
+        baseSepolia: process.env.PONDER_RPC_URL_84532
+          ? {
+              startBlock: 1128910,
+            }
+          : undefined,
+        scroll: process.env.PONDER_RPC_URL_534352
+          ? {
+              startBlock: 17,
+            }
+          : undefined,
+        scrollSepolia: process.env.PONDER_RPC_URL_534351
+          ? {
+              startBlock: 91,
+            }
+          : undefined,
+        opBnb: process.env.PONDER_RPC_URL_204
+          ? {
+              startBlock: 1125290,
+            }
+          : undefined,
+        opBnbTestnet: process.env.PONDER_RPC_URL_5611
+          ? {
+              startBlock: 2108065,
+            }
+          : undefined,
+        kroma: process.env.PONDER_RPC_URL_255
+          ? {
+              startBlock: 5197622,
+            }
+          : undefined,
+        kromaTestnet: process.env.PONDER_RPC_URL_2358
+          ? {
+              startBlock: 7783408,
+            }
+          : undefined,
+        avalancheC: process.env.PONDER_RPC_URL_43114
+          ? {
+              startBlock: 28865255,
+            }
+          : undefined,
+        avalancheFuji: process.env.PONDER_RPC_URL_43113
+          ? {
+              startBlock: 20797033,
+            }
+          : undefined,
+        zora: process.env.PONDER_RPC_URL_7777777
+          ? {
+              startBlock: 5223236,
+            }
+          : undefined,
+        zoraSepolia: process.env.PONDER_RPC_URL_999999999
+          ? {
+              startBlock: 3465249,
+            }
+          : undefined,
       },
     },
     'EntryPoint0.7': {
       abi: entryPoint07Abi,
       address: ENTRYPOINT_0_7_ADDRESS,
       network: {
-        ethereum: { startBlock: 19274877 },
-        goerli: { startBlock: 10568981 },
-        sepolia: { startBlock: 5328753 },
-        bsc: { startBlock: 36326227 },
-        bscTestnet: { startBlock: 38058129 },
-        polygon: { startBlock: 53784988 },
-        polygonMumbai: { startBlock: 46345465 },
-        opMainnet: { startBlock: 116453058 },
-        opSepolia: { startBlock: 8448043 },
-        arbitrum: { startBlock: 182947891 },
-        arbitrumSepolia: { startBlock: 16853333 },
-        gnosis: { startBlock: 32559861 },
-        celo: { startBlock: 24242863 },
-        celoAlfajores: { startBlock: 22698902 },
-        linea: { startBlock: 2506675 },
-        base: { startBlock: 10920348 },
-        baseSepolia: { startBlock: 6556474 },
-        scroll: { startBlock: 3591634 },
-        scrollSepolia: { startBlock: 3061869 },
-        opBnb: { startBlock: 17130896 },
-        avalancheC: { startBlock: 41958878 },
-        avalancheFuji: { startBlock: 30305092 },
-        zora: { startBlock: 11017288 },
-        zoraSepolia: { startBlock: 5324224 },
+        ethereum: process.env.PONDER_RPC_URL_1
+          ? {
+              startBlock: 19274877,
+            }
+          : undefined,
+        goerli: process.env.PONDER_RPC_URL_5
+          ? {
+              startBlock: 10568981,
+            }
+          : undefined,
+        sepolia: process.env.PONDER_RPC_URL_11155111
+          ? {
+              startBlock: 5328753,
+            }
+          : undefined,
+        bsc: process.env.PONDER_RPC_URL_56
+          ? {
+              startBlock: 36326227,
+            }
+          : undefined,
+        bscTestnet: process.env.PONDER_RPC_URL_97
+          ? {
+              startBlock: 38058129,
+            }
+          : undefined,
+        polygon: process.env.PONDER_RPC_URL_137
+          ? {
+              startBlock: 53784988,
+            }
+          : undefined,
+        polygonMumbai: process.env.PONDER_RPC_URL_80001
+          ? {
+              startBlock: 46345465,
+            }
+          : undefined,
+        opMainnet: process.env.PONDER_RPC_URL_10
+          ? {
+              startBlock: 116453058,
+            }
+          : undefined,
+        opSepolia: process.env.PONDER_RPC_URL_11155420
+          ? {
+              startBlock: 8448043,
+            }
+          : undefined,
+        arbitrum: process.env.PONDER_RPC_URL_42161
+          ? {
+              startBlock: 182947891,
+            }
+          : undefined,
+        arbitrumSepolia: process.env.PONDER_RPC_URL_421614
+          ? {
+              startBlock: 16853333,
+            }
+          : undefined,
+        gnosis: process.env.PONDER_RPC_URL_100
+          ? {
+              startBlock: 32559861,
+            }
+          : undefined,
+        celo: process.env.PONDER_RPC_URL_42220
+          ? {
+              startBlock: 24242863,
+            }
+          : undefined,
+        celoAlfajores: process.env.PONDER_RPC_URL_44787
+          ? {
+              startBlock: 22698902,
+            }
+          : undefined,
+        linea: process.env.PONDER_RPC_URL_59144
+          ? {
+              startBlock: 2506675,
+            }
+          : undefined,
+        base: process.env.PONDER_RPC_URL_8453
+          ? {
+              startBlock: 10920348,
+            }
+          : undefined,
+        baseSepolia: process.env.PONDER_RPC_URL_84532
+          ? {
+              startBlock: 6556474,
+            }
+          : undefined,
+        scroll: process.env.PONDER_RPC_URL_534352
+          ? {
+              startBlock: 3591634,
+            }
+          : undefined,
+        scrollSepolia: process.env.PONDER_RPC_URL_534351
+          ? {
+              startBlock: 3061869,
+            }
+          : undefined,
+        opBnb: process.env.PONDER_RPC_URL_204
+          ? {
+              startBlock: 17130896,
+            }
+          : undefined,
+        avalancheC: process.env.PONDER_RPC_URL_43114
+          ? {
+              startBlock: 41958878,
+            }
+          : undefined,
+        avalancheFuji: process.env.PONDER_RPC_URL_43113
+          ? {
+              startBlock: 30305092,
+            }
+          : undefined,
+        zora: process.env.PONDER_RPC_URL_7777777
+          ? {
+              startBlock: 11017288,
+            }
+          : undefined,
+        zoraSepolia: process.env.PONDER_RPC_URL_999999999
+          ? {
+              startBlock: 5324224,
+            }
+          : undefined,
       },
     },
   },
